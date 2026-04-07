888d12ddc9a65a3d1ed04958bb9806881072d9aa
Harshvardhan <hkamble1@student.gsu.edu>
2026-04-06 20:31:26 -0400

    wire firebase init in main and launch inventory screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/inventory_screen.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const InventoryApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class InventoryApp extends StatelessWidget {
+  const InventoryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Inventory Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(centerTitle: true),
       ),
+      home: const InventoryScreen(),
     );
   }
 }
